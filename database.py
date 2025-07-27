e70fe5aae31fd516bcbd7cb7413213ac30621bcf
mikecurious <mikkohbrayoh@gmail.com>
2025-07-27 20:58:40 +0300

    database config

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
+# Database configuration with environment variables
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "R7n#xE!dG2")
+DB_HOST = os.getenv("DB_HOST", "dedes.czgk0wyoco08.us-east-1.rds.amazonaws.com")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "dedes")
 
-DATABASE_URL= "mysql+pymysql://root:@localhost:3306/pos_system"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 
